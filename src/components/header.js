eabba8e033aec1556801411eccc7064db29f273e
milos-prince <michael@freedomgopher.com>
2020-06-14 13:00:30 -0400

    menu animation

--- src/components/header.js
+++ src/components/header.js
@@ -5,10 +5,10 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header>
     <div className="headerDesktopLHS">
-    <Link to="/about/" activeClassName="activeLink">about</Link>
-    <Link to="/family-arbitration/" activeClassName="activeLink">family arbitration</Link>
-    <Link to="/labour-and-commercial/" activeClassName="activeLink">labour and commercial</Link>
-    <Link to="/contact/" activeClassName="activeLink">contact</Link>
+    <div className="linkUnderlineWrapper"><Link to="/about/" className="linkUnderline" activeClassName="activeLink">about</Link></div>
+    <div className="linkUnderlineWrapper"><Link to="/family-arbitration/" className="linkUnderline" activeClassName="activeLink">family arbitration</Link></div>
+    <div className="linkUnderlineWrapper"><Link to="/labour-and-commercial/" className="linkUnderline" activeClassName="activeLink">labour and commercial</Link></div>
+    <div className="linkUnderlineWrapper"><Link to="/contact/" className="linkUnderline" activeClassName="activeLink">contact</Link></div>
     </div>
     <div className="headerDesktopRHS">
     <Link to="/">home</Link>
