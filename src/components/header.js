e428fe2d37c9a2bd6055ee0d9f642b4709dbe52a
milos-prince <michael@freedomgopher.com>
2020-06-14 16:12:28 -0400

    logo and photos

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,12 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import logo from "../images/LBP.svg"
 
 const Header = ({ siteTitle }) => (
   <header>
     <div className="headerDesktop">
-    <Link to="/">home</Link>
+    <Link to="/"><img src={logo} className="logo"/></Link>
     <div className="linkUnderlineWrapper"><Link to="/about/" className="linkUnderline" activeClassName="activeLink">about</Link></div>
     <div className="linkUnderlineWrapper"><Link to="/family-arbitration/" className="linkUnderline" activeClassName="activeLink">family arbitration</Link></div>
     <div className="linkUnderlineWrapper"><Link to="/labour-and-commercial/" className="linkUnderline" activeClassName="activeLink">labour and commercial</Link></div>
