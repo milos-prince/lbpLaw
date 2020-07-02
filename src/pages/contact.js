8778309759aa691eb0c70343074b7ac850c93791
milos-prince <michael@freedomgopher.com>
2020-07-02 10:27:07 -0400

    forms..

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -14,19 +14,19 @@ const NotFoundPage = () => (
     <form name="contact" method="post" data-netlify="true" netlify-honeypot="bot-field">
     <input type="hidden" name="form-name" value="contact" />
     <p className="d-none">
-    <label>This field is not for humans <input name="bot-field" /></label>
+    <label>This field is not for hum.ans <input name="bot-field" /></label>
   </p>
   <div class="form-group">
-    <input type="name" class="form-control" id="exampleInputPassword1" placeholder="Your name" />
+    <input name="name" type="name" class="form-control" placeholder="Your name" />
   </div>
   <div class="form-group">
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="E-mail address" />
+    <input name="email" type="email" class="form-control" aria-describedby="emailHelp" placeholder="E-mail address" />
   </div>
   <div class="form-group">
-    <input type="name" class="form-control" id="exampleInputPassword1" placeholder="Subject" />
+    <input name="subject" type="text" class="form-control"  placeholder="Subject" />
   </div>
   <div class="form-group">
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Your message"></textarea>
+  <textarea name="message" class="form-control" rows="3" placeholder="Your message"></textarea>
   </div>
   <h5 className="disclaimerHeading">Cautionary note:</h5>
   <p className="disclaimer">If you are not currently a client of Leslie Belloc-Pinder, any information you communicate in your email is not protected by lawyer/client confidentiality, nor is a lawyer/client relationship established by our receipt of this email. Before we can discuss any legal matter, we must ensure that we do not have a conflict of interest. Please do not send us any confidential information in your e-mail.</p>
