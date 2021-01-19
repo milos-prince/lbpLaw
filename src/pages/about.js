d1981da22a89d0736bebaa7ba1e4edd8d0768850
milos-prince <mike.pinder@gmail.com>
2021-01-18 22:26:57 -0500

    updated resume

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,7 +2,7 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import aboutPicture from "../images/aboutPicture.jpg"
-import resume from "../../static/leslie-resume.pdf"
+import resume from "../../static/leslie-resume2.pdf"
 
 const IndexPage = () => (
   <Layout>
