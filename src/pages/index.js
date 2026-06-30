a387e3becbb2be1bfa0d2783222f6edf60982980
Michael Pinder <mike.pinder@gmail.com>
2026-06-29 18:50:48 -0600

    changed front page

--- src/pages/index.js
+++ src/pages/index.js
@@ -30,7 +30,7 @@ const IndexPage = () => (
         </div>
         <div className="d-flex column colB">
         <div className="colBTop">
-          <p className="mb-0">Arbitration and mediation are forms of Alternate Dispute Resolution (ADR) intended to avoid the high cost and unpredictable outcomes that could result from going to court. But mediation requires parties to agree in the end, and if there is no agreement, there is no concrete result. Arbitration, also referred to as adjudication, is a process that results in a decision made by a neutral person who conducts a fair hearing outside the court process.</p>
+          <p className="mb-0">Administrative law decision-making requires experience and a commitment to fairness. As a Chartered Arbitrator focused on tribunal adjudication since 2008, I bring over 40 years of litigation experience to complex labour and employment, immigration, and environmental disputes. I will provide a transparent, approachable process anchored by neutrality and adaptability. You can rely on a fair hearing and a well-reasoned written decision delivered promptly.</p>
         </div>
         <div id="homepagePhoto2"></div>
         </div>
